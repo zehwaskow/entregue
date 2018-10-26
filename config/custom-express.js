222a08b025d624ba24dbd9d9e74082e1a088698a
Juliano Pereira Lima <julianodgtz@gmail.com>
2018-10-25 22:29:54 -0300

    Add body parser via json or URL on custom-express

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var consign = require("consign");
+var bodyParser = require("body-parser");
 
 module.exports = () => {
   var app = express();
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
   consign()
     .include("controllers")

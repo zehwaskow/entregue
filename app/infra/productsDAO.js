a7e0ba5dbf40e82c0bd9cffe7dcef249a32f388f
Juliano Pereira Lima <julianodgtz@gmail.com>
2018-10-25 21:30:22 -0300

    Update package-lock.json

--- app/infra/productsDAO.js
+++ app/infra/productsDAO.js
@@ -1,16 +1,14 @@
-function ProdutosDAO(connection) {
-    this._connection = connection;
+function ProductsDAO(connection) {
+	this._connection = connection;
 }
 
-ProdutosDAO.prototype.lista = function (callback) {
-    this._connection.query('select * from produtos', callback);
+ProductsDAO.prototype.lista = function (callback) {
+	this._connection.query('select * from produtos', callback);
 };
 
-ProdutosDAO.prototype.salva = function (produto, callback) {
-    this._connection.query('insert into produtos set ?', produto, callback);
-}
-
-module.exports = () => {
-    return ProdutosDAO;
+ProductsDAO.prototype.salva = function (produto, callback) {
+	this._connection.query('insert into produtos set ?', produto, callback);
 };
+
+module.exports = () => ProductsDAO;
 
